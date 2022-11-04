538a9f726f29112efa0867f1438351959f439252
samuel.santos <samuel.santos@elo.inf.br>
2022-11-04 15:11:10 -0300

    commit teste

--- src/test/java/com/mini/autorizador/service/CartaoServiceTest.java
+++ src/test/java/com/mini/autorizador/service/CartaoServiceTest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @RunWith(SpringRunner.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class CartaoServicoTest {
+public class CartaoServiceTest {
 
     @Autowired(required = true)
     private CartaoService serviceCartao;
